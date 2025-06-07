b9b710f55306cb59fc54ffcbc105c181e2f864d4
NP-Sinh <npsinh6103@gmail.com>
2025-06-07 08:35:39 +0700

    .

--- JwtAuth/Controllers/AuthController.cs
+++ JwtAuth/Controllers/AuthController.cs
@@ -10,26 +10,38 @@ namespace JwtAuth.Controllers
         {
             return View();
         }
+        [HttpPost("login")]
         public IActionResult Login()
         {
             return View();
         }
+        [HttpPost("register")]
         public IActionResult Register()
         {
             return View();
         }
+        [HttpPost("logout")]
         public IActionResult Logout()
         {
             return View();
         }
+        [HttpPost("info")]
+        [Authorize]
         public IActionResult Info()
         {
             return View();
         }
+        [HttpPost("testauth")]
         [Authorize]
         public IActionResult TestAuth()
         {
             // nếu đăng nhập rồi thì mới truy cập được, nếu chưa đăng nhập thì lỗi 401 Unauthorized
+            return View();
+        }
+        [HttpGet("admin")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminOnly()
+        {           
             return View();
         }
     }
